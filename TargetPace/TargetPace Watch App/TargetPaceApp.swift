02e06d75c31460a0ee5c949a908d01b4ce6c0c0f
mbenz12 <mbenz10120112@gmail.com>
2024-04-04 06:42:18 +0300

    target pace implemented

---
+++ TargetPace/TargetPace Watch App/TargetPaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetPaceApp.swift
+//  TargetPace Watch App
+//
+//  Created by Vladyslav on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TargetPace_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TargetPace/TargetPace Watch App/TargetPaceSettingsView.swift
@@ -0,0 +1,42 @@
+//
+//  TargetPaceSettingsView.swift
+//  TargetPace Watch App
+//
+//  Created by Vladyslav on 04.04.2024.
+//
+
+import SwiftUI
+
+struct TargetPaceSettingsView: View {
+    @Binding var selectedPace: Double
+    @Binding var targets: [TargetPace]
+    @Binding var selectedTab: Int
+    
+    let paceOptions: [Double] = Array(stride(from: 0.0, through: 10.0, by: 0.1))
+
+    var body: some View {
+        VStack {
+            Picker("Select Target Pace", selection: $selectedPace) {
+                ForEach(paceOptions, id: \.self) { pace in
+                    Text(String(format: "%.1f", pace))
+                        .font(.system(size: pace == self.selectedPace ? 36: 24))
+                }
+            }
+            .pickerStyle(WheelPickerStyle())
+            .frame(height: 120)
+
+            Button(action: {
+                targets.append(TargetPace(pace: selectedPace))
+                selectedTab = targets.count
+            }) {
+                Text("Add Target")
+            }
+        }
+    }
+}
+
+struct TargetPaceSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TargetPaceSettingsView(selectedPace: .constant(6.0), targets: .constant([]), selectedTab: .constant(0))
+    }
+}
